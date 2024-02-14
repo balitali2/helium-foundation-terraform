a91017b6b25ac162a2be6037dbd2bcdb25e5af44
Darwin Rinderer <darwin@helium.foundation>
2024-02-14 12:33:26 -0500

    Updating indentation

--- modules/k8s/cluster-auth.tf
+++ modules/k8s/cluster-auth.tf
@@ -1,4 +1,4 @@
 resource "kubectl_manifest" "cluster-auth" {
   count      =  length(data.kubectl_path_documents.cluster-auth.documents)
-  yaml_body  = element(data.kubectl_path_documents.cluster-auth.documents, count.index)
+  yaml_body  =  element(data.kubectl_path_documents.cluster-auth.documents, count.index)
 }
