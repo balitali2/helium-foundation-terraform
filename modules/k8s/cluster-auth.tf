00c3a268209381388c88d59c0d0ec24981c26dc1
Darwin Rinderer <darwin@helium.foundation>
2024-02-14 12:32:34 -0500

    Adding read-only iam principal access

---
+++ modules/k8s/cluster-auth.tf
@@ -0,0 +1,4 @@
+resource "kubectl_manifest" "cluster-auth" {
+  count      =  length(data.kubectl_path_documents.cluster-auth.documents)
+  yaml_body  = element(data.kubectl_path_documents.cluster-auth.documents, count.index)
+}
